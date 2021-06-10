15d9015cee0e2413e0798191780ec0ab22191814
Pavel Lobashov <shockwavenn@gmail.com>
2021-06-10 12:26:26 +0300

    Fix rubocop-rspec warning

--- spec/mail_message_spec.rb
+++ spec/mail_message_spec.rb
@@ -4,7 +4,8 @@ require 'spec_helper'
 
 RSpec.describe 'MailMessage' do
   it 'check compare of two same mail messages' do
-    expect(OnlyofficeGmailHelper::MailMessage.new('test', 'test'))
-      .to eq(OnlyofficeGmailHelper::MailMessage.new('test', 'test'))
+    message1 = OnlyofficeGmailHelper::MailMessage.new('test', 'test')
+    message2 = OnlyofficeGmailHelper::MailMessage.new('test', 'test')
+    expect(message1).to eq(message2)
   end
 end
